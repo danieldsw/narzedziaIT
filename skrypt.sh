83f104f94c87eba6e62066815d17b521ef165319
danieldsw <pl.danielpycha1@gmail.com>
2022-07-15 21:32:44 +0200

    add

--- skrypt.sh
+++ skrypt.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #test
-plik=$1
-liczba=$2
+arg1=$1
+arg2=$2
 
-if [ -z "$liczba" ]
+if [ -z "$arg2" ]
 then
-	liczba=100
+	arg2=100
 fi
 
-if [ "$plik" == "--date" ]
+if [ "$arg1" == "--date" ] || [ "$arg1" == "-d" ];
 then
 echo $(date '+%Y-%m-%d')
 read
@@ -17,10 +17,10 @@ fi
 
 
 
-if [ "$plik" == "--logs" ]
+if [ "$arg1" == "--logs" ] || [ "$arg1" == "-l" ];
 then
 	
-	for (( i=1; i<=$liczba; i++ ))
+	for (( i=1; i<=$arg2; i++ ))
 	do 
 		touch log$i.txt 
 		echo log$i.txt > log$i.txt
@@ -30,7 +30,7 @@ then
 read
 fi
 
-if [ "$plik" == "--help" ]
+if [ "$arg1" == "--help" ] || [ "$arg1" == "-h" ];
 then
 	echo "Wszysktie dostepne flagi:"
 	echo "--date   pokazuje aktualna date"
