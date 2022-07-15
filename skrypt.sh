dacdbd0503bad539ac6f28519b5537d22480e99a
danieldsw <pl.danielpycha1@gmail.com>
2022-07-15 22:22:49 +0200

    add

--- skrypt.sh
+++ skrypt.sh
@@ -7,11 +7,16 @@ then
 	echo "--date   pokazuje aktualna date"
 	echo "--logs   tworzy 100 plikow txt o nazwie logx"
 	echo "--logs <liczba>   tworzy wybrana ilosc plikow txt"
+	echo "--init   tworzy repozytorium w folderze"
+	echo "--error  tworzy 100 plikow txt o nazwie errorx"
+	echo "--error <liczba>   tworzy wybrana ilosc plikow txt"
 	echo " "
 	echo "-h   pokazuje wszystkie dostepne opcje"
 	echo "-d   pokazuje aktualna date"
 	echo "-l   tworzy 100 plikow txt o nazwie logx"
 	echo "-l <liczba>   tworzy wybrana ilosc plikow txt"
+	echo "-e  tworzy 100 plikow txt o nazwie errorx"
+	echo "-e <liczba>   tworzy wybrana ilosc plikow txt"
 	read
 fi 
 
